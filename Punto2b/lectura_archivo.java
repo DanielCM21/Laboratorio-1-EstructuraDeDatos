6257e96242b81944da17db65f92a91a8fa53dab2
DanielCM21 <edcastaneda@uninorte.edu.co>
2022-09-02 11:04:58 -0500

    timer

--- Punto2b/lectura_archivo.java
+++ Punto2b/lectura_archivo.java
@@ -1,8 +1,6 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class lectura_archivo {
@@ -10,6 +8,7 @@ public class lectura_archivo {
     private static String[] datos;
     public static void main(String[] args) {
         
+        long inicio = System.nanoTime();
         String lineas;
         try
         {
@@ -24,27 +23,8 @@ public class lectura_archivo {
         {
             e.printStackTrace();
         }
-    }
-}
-
-class StopWatch{
-
-    Timer timer;
-
-    public StopWatch(int seconds) {
-        timer = new Timer();
-        timer.schedule(new StopTask(), seconds * 1000);
-    }
-
-    public static void main(String[] args) {
-        new StopWatch(10);
-        System.out.println("StopWatch Started.");
-    }
-
-    class StopTask extends TimerTask {
-        public void run() {
-            System.out.println("Time Up!");
-            timer.cancel();
-        }
+        long fin = System.nanoTime() - inicio;
+        float segundos = (float)fin/1000000000; 
+        System.out.println("Este proceso tuvo una duración de:" + segundos + " segundos");
     }
 }
